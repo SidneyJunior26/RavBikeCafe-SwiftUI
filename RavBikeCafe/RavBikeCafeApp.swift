2d87c30876c39c844209efe1982f1bcb68745df1
Sidney <sidneyjunior26@gmail.com>
2024-05-30 11:48:33 -0300

    Initial Commit

---
+++ RavBikeCafe/RavBikeCafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RavBikeCafeApp.swift
+//  RavBikeCafe
+//
+//  Created by Sidney Junior on 30/05/24.
+//
+
+import SwiftUI
+
+@main
+struct RavBikeCafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
